5651966560c662be01615862807f9e5902ff8a9b
Andrew Peters <dpeters@myocv.com>
2017-02-21 00:14:20 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -64,32 +64,52 @@ class Sample(object):
         result = base ** exponent
         return result
     
-    def integrate(self, lowBound, highBound, n, f):
-        epsilon = 0.001
-        simpsonOld = 0.0
-        simpsonNew = epsilon
-        s = 4
-        coefficient = 0
-        temp = 0.0
-        while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
-            simpsonOld = simpsonNew
-            w = (highBound - lowBound) / s
-            for i in range(0, s):
-                if i % 2 == 0:
-                    coefficient = 4
-                if i % 2 == 1:
-                    coefficient = 2
-                if i == 0 or i == s:
-                    coefficient = 1
+    # def integrate(self, lowBound, highBound, n, f):
+    #     epsilon = 0.001
+    #     simpsonOld = 0.0
+    #     simpsonNew = epsilon
+    #     s = 4
+    #     coefficient = 0
+    #     temp = 0.0
+    #     while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
+    #         simpsonOld = simpsonNew
+    #         w = (highBound - lowBound) / s
+    #         for i in range(0, s):
+    #             if i % 2 == 0:
+    #                 coefficient = 4
+    #             if i % 2 == 1:
+    #                 coefficient = 2
+    #             if i == 0 or i == s:
+    #                 coefficient = 1
+    #
+    #             temp += coefficient * f(lowBound + (w*i), n)
+    #
+    #         simpsonNew = (w/3) * temp
+    #         s = s * 2
+    #
+    #     return simpsonNew
 
-                temp += coefficient * f(lowBound + (w*i), n)
-
-            simpsonNew = (w/3) * temp
-            s = s * 2
-
-        return simpsonNew
-
-
+    def Simpson(f, a, b, n):
+        if a > b:
+            print 'Incorrect bounds'
+            return None
+        if n%2 != 0: # also an 'if' because both tests are NOT# mutually exclusive
+            print 'Invalid choice of n'
+            return None
+        else:
+    	    h = (b - a)/float(n) # need to cast 'n' as float in order to avoid
+            # integer division
+    	    sum1 = 0
+    	    for i in range(1, n/2 + 1):
+        	    sum1 += f(a + (2*i - 1)*h)
+    	    sum1 *= 4
+    	    sum2 = 0
+    	    for i in range(1, n/2): # range must be ints: range() integer
+                #end argument expected, got float.
+        	    sum2 += f(a + 2*i*h)
+    	    sum2 *= 2
+    	    approx = (b - a)/(3.0*n)*(f(a) + f(b) + sum1 + sum2)
+    	    return approx
 
 
         
