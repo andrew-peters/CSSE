fd2587439a915419ba44518b5c3aeea13d93773e
Andrew Peters <dpeters@myocv.com>
2017-02-20 23:31:07 -0600

    Red Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -91,6 +91,64 @@ class Sample(object):
 
 
 
+    def integrate(self, lowBound, highBound, n, f):
+        """
+        integrate returns the integral of f  with n degrees of freedom from lowBound to highBound.
+        :param lowBound: The lower bound of the integration. Typically 0.
+        :param highBound: t, the upper bound of the integration. >= 0.
+        :param n: The number of degrees of freedom the distribution has.
+        :param f: The function to integrate over, with u and n.
+        :return: The area under f between lowBound and highBound, approximated with Simpson's rule.
+        """
+        epsilon = 0.0001
+        simpsonOld = 0.0
+        simpsonNew = epsilon
+        s = 4
+        while abs(simpsonNew - simpsonOld) / simpsonNew > epsilon:
+            simpsonOld = simpsonNew
+            simpsonNew = self.calculateSimpson(lowBound, highBound, n, f, s)
+            s *= 2
+        return simpsonNew
+
+    def calculateSimpson(self, lowBound, highBound, n, f, s):
+        """
+        calculateSimpson returns the integral of f with n degrees of freedom from lowBound to highBound, broken
+        into s partitions.
+        :param lowBound:    numeric mandatory validated
+        :param highBound:   numeric .GE. lowBound mandatory validated
+        :param n:           numeric .GE. 2 and .LT. 30 mandatory validated
+        :param f:           function(self, float, integer) -> float mandatory validated
+        :param s:           integer .GE. 1 mandatory validated
+        :return: float, the integral of f, on s partitions
+        """
+        w = (highBound - lowBound) / s
+        total = 0.0
+        for i in range(0, s+1):
+            total += self.getCoefficient(i, s) * f(lowBound + w * i, n)
+
+        return (w / 3) * total
+
+    def getCoefficient(self, index, numPartitions):
+        """
+        getCoefficient returns the coefficient for the indexth element of the partitions.
+        If this is the first or last coefficient, this is 1. Otherwise, it alternates between 2 and 4.
+        :param index: Which element of the partitions to calculate the coefficient for, from 0 (the first partition)
+        up to numPartitions (inclusive).
+        :param numPartitions: The number of partitions to break the function into.
+        :return: An integer, one of 1, 2, or 4, the appropriate coefficient for this partition.
+        """
+
+        # Special handling for the boundaries.
+        if index == 0 or index == numPartitions:
+            return 1
+
+        # Otherwise, every-other has coefficient 2, starting with index 1 (odd indices)
+        if index % 2 == 1:
+            return 2
+        # With all other coefficients (even indices) are 4.
+        else:
+            return 4
+
         
         
     
