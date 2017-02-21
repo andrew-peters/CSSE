345feeb87276c8a24541ea70f3ab8181a5798516
Andrew Peters <dpeters@myocv.com>
2017-02-21 11:34:52 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -74,6 +74,7 @@ class Sample(object):
         while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
             simpsonOld = simpsonNew
             w = (highBound - lowBound) / s
+            simpsonNew = f(lowBound, n)
             for i in range(1, s+1):
 
                 if i == 1 or i == s+1:
