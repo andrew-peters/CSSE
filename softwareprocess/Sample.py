77f337aa92dcac78c792759a7e02f73f6563dba7
Andrew Peters <dpeters@myocv.com>
2017-02-21 11:30:25 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -89,28 +89,7 @@ class Sample(object):
     #
     #     return simpsonNew
 
-    def integrate(f, a, b, n):
-        if a > b:
-            print 'Incorrect bounds'
-            return None
-        if n%2 != 0: # also an 'if' because both tests are NOT# mutually exclusive
-            print 'Invalid choice of n'
-            return None
-        else:
-    	    h = (b - a)/float(n) # need to cast 'n' as float in order to avoid
-            # integer division
-    	    sum1 = 0
-    	    for i in range(1, n/2 + 1):
-        	    sum1 += f(a + (2*i - 1)*h)
-    	    sum1 *= 4
-    	    sum2 = 0
-    	    for i in range(1, n/2): # range must be ints: range() integer
-                #end argument expected, got float.
-        	    sum2 += f(a + 2*i*h)
-    	    sum2 *= 2
-    	    approx = (b - a)/(3.0*n)*(f(a) + f(b) + sum1 + sum2)
-    	    return approx
-
+    
 
         
     
