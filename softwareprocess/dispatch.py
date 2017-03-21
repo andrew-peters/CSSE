14ef021cd1970d0fac9fa30a005699be7064d6bb
Andrew Peters <dpeters@myocv.com>
2017-03-21 12:00:39 -0500

    Update.

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -117,8 +117,8 @@ def format_altitude(altitude):
     return outputAlt
 
 
-inputval = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-inputval2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-inputval3 = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-output = dispatch(inputval3)
-print output
+# inputval = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+# inputval2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+# inputval3 = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+# output = dispatch(inputval3)
+# print output
