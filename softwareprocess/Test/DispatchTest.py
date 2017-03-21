f0bf6bdc09c02cb8d28944cef5ec660d310cff9e
Andrew Peters <dpeters@myocv.com>
2017-03-21 11:55:58 -0500

    Green Light |

--- softwareprocess/Test/DispatchTest.py
+++ softwareprocess/Test/DispatchTest.py
@@ -22,4 +22,7 @@ class DispatchTest(unittest.TestCase):
         self.assertTrue(output == {'error':'parameter is not a dictionary'})
 
     def test_dispatch_invalid_observation(self):
-        output = DT.dispatch()
+        output = DT.dispatch({'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural',
+                              'op': 'adjust', 'temperature': '71'})
+        self.assertTrue(output == {'temperature': '71', 'height': '6', 'pressure': '1010', 'horizon': 'natural',
+                                   'error': 'degrees are out of range, should be between 0 and 90', 'observation': '101d15.2', 'op': 'adjust'})
--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -119,5 +119,6 @@ def format_altitude(altitude):
 
 inputval = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
 inputval2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-output = dispatch(inputval2)
+inputval3 = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+output = dispatch(inputval3)
 print output
