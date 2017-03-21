a3c6a3808ed1133b12abdb65d70b95848faca08b
Andrew Peters <dpeters@myocv.com>
2017-03-21 16:49:16 -0500

    Refactor |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -120,5 +120,5 @@ def format_altitude(altitude):
 # inputval = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000', 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
 # inputval2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
 # inputval3 = {'observation': '101d15.2', 'height': '6', 'pressure': '1010', 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-# output = dispatch(inputval3)
+# output = dispatch(inputval)
 # print output
