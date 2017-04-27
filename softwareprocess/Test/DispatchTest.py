e67ee5b8135280f7e0c4c2816cb321573383db48
Andrew Peters <dpeters@myocv.com>
2017-04-27 18:06:07 -0500

    Green Light |

--- softwareprocess/Test/DispatchTest.py
+++ softwareprocess/Test/DispatchTest.py
@@ -69,6 +69,6 @@ class DispatchTest(unittest.TestCase):
 
     def test300_01_ShouldRetrunError(self):
         input = {'op': 'correct', 'lat': '5d87'}
-        output = {'error': 'Missing mandatory information', 'op': 'correct'}
+        output = {'op': 'correct', 'error': 'Missing mandatory information'}
         self.assertDictEqual(DT.dispatch(input), output)
 
